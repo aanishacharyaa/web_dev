be630fdee7f3457931fb8e7e015d34974b130391
aanishacharyaa <60659030+aanishacharyaa@users.noreply.github.com>
2022-01-03 21:56:26 +0530

    Add files via upload

---
+++ img_data_cdn.js
@@ -0,0 +1,3 @@
+var img_data_cdn = {
+    8760754855392222304 : { i1 : "https://drive.google.com/uc?id=1G6KQG9zmCyA9I0I-jJwJ3FxTvqYJNpI3" , i2 : "https://drive.google.com/uc?id=1uWRco8pwxr0BJeKdRsNqojbijfIXyzd_"}
+} 
